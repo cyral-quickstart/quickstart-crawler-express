fe6c9f017c8ac9dde5141e1d8e4041c9987ecf4c
Justin Rich <jrich@cyral.com>
2022-11-01 08:59:31 -0700

    simlify install script for testing

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# install docker
-
-# create directories with propper permissions
-
-# default location for configs
-mkdir /etc/cyral
-chmod 
-# symlink scripts
 ln -s "${PWD}/crawler" /usr/bin/crawler
 ln -s "${PWD}/crawler-job" /usr/bin/crawler-job
